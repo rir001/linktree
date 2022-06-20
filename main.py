046e61564de7f81888e57ed89d17807bda2aafd9
rir001 <riveraldrv@gmail.com>
2022-06-20 12:48:32 -0400

    basic

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/instagram', methods=['GET'])
+def instagram():
+    return render_template('instagram.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
